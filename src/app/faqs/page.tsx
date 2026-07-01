be74851eee288d4f257c364b97db4abcf0ab9b18
mantanova-87 <cdt.mantavyakumar@gmail.com>
2026-07-02 02:25:29 +0530

    feat: Add FAQ component and page for frequently asked questions

---
+++ src/app/faqs/page.tsx
@@ -0,0 +1,10 @@
+import { FAQ } from "@/features/faqs";
+export default function faqs()
+{
+    return(
+        <>
+            <FAQ/>
+        </>
+        
+    )
+}
---
+++ src/features/faqs/faq.tsx
@@ -0,0 +1,12 @@
+import { Section } from "@/components/common/section";
+import { Container } from "@/components/common/container";
+ export function FAQ (){
+    return(
+        <Section>
+            <Container>
+                
+            </Container>
+        </Section>
+
+    );
+ }
